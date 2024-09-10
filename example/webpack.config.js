65199cd830fd67c7056ce6708ea8b341abc5f545
Davide Volta <davide.volta@oneam.it>
2024-09-10 12:06:54 +0200

    fix: fix example project

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,19 +2,21 @@ const createConfigAsync = require('@expo/webpack-config');
 const path = require('path');
 
 module.exports = async (env, argv) => {
-  const config = await createConfigAsync(
-    {
-      ...env,
-      babel: {
-        dangerouslyAddModulePathsToTranspile: ['expo-ios-color-picker'],
-      },
-    },
-    argv
-  );
-  config.resolve.modules = [
-    path.resolve(__dirname, './node_modules'),
-    path.resolve(__dirname, '../node_modules'),
-  ];
+    const config = await createConfigAsync(
+        {
+            ...env,
+            babel: {
+                dangerouslyAddModulePathsToTranspile: [
+                    '@one-am/expo-ios-color-picker',
+                ],
+            },
+        },
+        argv
+    );
+    config.resolve.modules = [
+        path.resolve(__dirname, './node_modules'),
+        path.resolve(__dirname, '../node_modules'),
+    ];
 
-  return config;
+    return config;
 };
