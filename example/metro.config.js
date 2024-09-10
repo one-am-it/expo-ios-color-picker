fcdea1df15399c54b8b71049084a5a42ef9438ce
Davide Volta <davide.volta@oneam.it>
2024-09-10 10:50:24 +0200

    fix: fix metro.config.js config for example project, add .eslintignore

--- example/metro.config.js
+++ example/metro.config.js
@@ -8,27 +8,27 @@ const config = getDefaultConfig(__dirname);
 // To prevent the incompatible react-native between ./node_modules/react-native and ../node_modules/react-native,
 // excludes the one from the parent folder when bundling.
 config.resolver.blockList = [
-  ...Array.from(config.resolver.blockList ?? []),
-  new RegExp(path.resolve('..', 'node_modules', 'react')),
-  new RegExp(path.resolve('..', 'node_modules', 'react-native')),
+    ...Array.from(config.resolver.blockList ?? []),
+    new RegExp(path.resolve('..', 'node_modules', 'react')),
+    new RegExp(path.resolve('..', 'node_modules', 'react-native')),
 ];
 
 config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, './node_modules'),
-  path.resolve(__dirname, '../node_modules'),
+    path.resolve(__dirname, './node_modules'),
+    path.resolve(__dirname, '../node_modules'),
 ];
 
 config.resolver.extraNodeModules = {
-  'expo-ios-color-picker': '..',
+    '@one-am/expo-ios-color-picker': '..',
 };
 
 config.watchFolders = [path.resolve(__dirname, '..')];
 
 config.transformer.getTransformOptions = async () => ({
-  transform: {
-    experimentalImportSupport: false,
-    inlineRequires: true,
-  },
+    transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+    },
 });
 
 module.exports = config;
